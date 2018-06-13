670471f1f1ae5f28edc60b7ddbcf16f0dd389ac7
Damien SEDIRA <es.todo78@yahoo.fr>
2018-06-13 22:18:55 +0200

    FOSUser

---
+++ src/AppBundle/Form/UserType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+
+class UserType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstName', TextType::class, array('attr' => array('required' => true)))
+            ->add('lastName', TextType::class, array('attr' => array('required' => true)))
+            ->add('email', EmailType::class, array('attr' => array('required' => true)))
+            ->add('phone', TelType::class, array('attr' => array('required' => true)))
+            ->add('role', TextType::class, array('attr' => array('required' => true)));
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_user';
+    }
+
+
+}
