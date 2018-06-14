f0d69377bffbdde8d6d838556ced269ed829c94b
Cyril Do <cyrildo@yahoo.fr>
2018-06-14 16:00:35 +0200

    gettersetter AUE

--- src/AppBundle/Entity/ArtworkUserEmotion.php
+++ src/AppBundle/Entity/ArtworkUserEmotion.php
@@ -45,4 +45,86 @@ class ArtworkUserEmotion
      * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Emotion", inversedBy="aues")
      */
     private $emotion;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set artwork
+     *
+     * @param \AppBundle\Entity\Artwork $artwork
+     *
+     * @return ArtworkUserEmotion
+     */
+    public function setArtwork(\AppBundle\Entity\Artwork $artwork = null)
+    {
+        $this->artwork = $artwork;
+
+        return $this;
+    }
+
+    /**
+     * Get artwork
+     *
+     * @return \AppBundle\Entity\Artwork
+     */
+    public function getArtwork()
+    {
+        return $this->artwork;
+    }
+
+    /**
+     * Set user
+     *
+     * @param \AppBundle\Entity\User $user
+     *
+     * @return ArtworkUserEmotion
+     */
+    public function setUser(\AppBundle\Entity\User $user = null)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return \AppBundle\Entity\User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Set emotion
+     *
+     * @param \AppBundle\Entity\Emotion $emotion
+     *
+     * @return ArtworkUserEmotion
+     */
+    public function setEmotion(\AppBundle\Entity\Emotion $emotion = null)
+    {
+        $this->emotion = $emotion;
+
+        return $this;
+    }
+
+    /**
+     * Get emotion
+     *
+     * @return \AppBundle\Entity\Emotion
+     */
+    public function getEmotion()
+    {
+        return $this->emotion;
+    }
 }
